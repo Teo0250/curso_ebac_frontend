f890beca0d749bdee3dacd88a3831d040fc5e16e
Matheus Medeiros <mf.matheus00@gmail.com>
2022-12-02 08:05:12 -0300

    adicionando formulário do exercicio

---
+++ main.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+    $('#cpf').mask('000.000.000-00', {
+        placeholder: '000.000.000-00'
+    })
+
+    $('#CEP').mask('00000-000', {
+        placeholder: '00000-000'
+    })
+    
+    $('form').validate({
+        rules: {
+            name: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            cpf: {
+                required: true
+            },
+            endereco: {
+                required: false
+            },
+            CEP: {
+                required: true
+            }
+        },
+        submitHandler: function(form) {
+            console.log(form)
+        },
+        invalidHandler: function(event, validator) {
+            let incorrectFields = validator.numberOfInvalids();
+            if(incorrectFields) {
+                alert(`There ${incorrectFields} incorrect fields`)
+            }
+        }
+    })
+})
